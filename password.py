cbb1ca005f9d6e1ff2eccf43b62a5017159f29a6
Jason <Jason06052005@gmail.com>
2022-07-11 17:56:05 -0700

    weektwoexercise

---
+++ password.py
@@ -0,0 +1,12 @@
+secret_password = 'kittens'  # super top secret password
+password = input('Enter the secret password : ')
+if password == secret_password:
+    print('Welcome, authorized user')
+
+else:
+    print('Sorry, wrong password')
+
+# read_syllabus = input('Enter Y if you read the syllabus for there class: ')
+# if read_syllabus != 'Y':
+#     print('Please read the Syllabus carefully, there is important info in it. Thanks!')
+
